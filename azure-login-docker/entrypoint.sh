37620c0422110ecc23f410d59c989fc3abc140b4
Sumiran Aggarwal <suaggar@microsoft.com>
2019-07-10 15:24:03 +0530

    spike

--- azure-login-docker/entrypoint.sh
+++ azure-login-docker/entrypoint.sh
@@ -3,3 +3,4 @@
 set -e
 
 az login --service-principal -u "586a5f0f-3720-42b2-82ef-d4479d53a0a5" -p "/gk+ZqLp7E2PDrzuTvRg0Zju67ExLgAZ0wJisZaV6OM=" --tenant "72f988bf-86f1-41af-91ab-2d7cd011db47"
+az account list
