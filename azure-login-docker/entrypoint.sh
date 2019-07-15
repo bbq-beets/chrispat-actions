8acef5a1959201f84a840d3d6267242a39a26bfb
Sumiran Aggarwal <suaggar@microsoft.com>
2019-07-15 14:27:46 +0530

    docker task

---
+++ azure-login-docker/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+args
+RESOURCE_GROUP_NAME=$(az resource list -n "suaggarTest1" --resource-type "Microsoft.Web/Sites" --query '[0].resourceGroup' | xargs)
+echo `${RESOURCE_GROUP_NAME}` >&2
