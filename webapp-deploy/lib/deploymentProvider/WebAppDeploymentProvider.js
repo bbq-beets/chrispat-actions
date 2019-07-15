3159e29ca37dc0638d9408bac5b032f048a35036
Sumiran Aggarwal <suaggar@microsoft.com>
2019-07-15 17:53:51 +0530

    cleanup

--- webapp-deploy/lib/deploymentProvider/WebAppDeploymentProvider.js
+++ webapp-deploy/lib/deploymentProvider/WebAppDeploymentProvider.js
@@ -7,12 +7,19 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const KuduServiceUtility_1 = require("../common/RestUtilities/KuduServiceUtility");
 const azure_app_service_1 = require("../common/ArmRest/azure-app-service");
 const AzureAppServiceUtility_1 = require("../common/RestUtilities/AzureAppServiceUtility");
-const tl = require("azure-pipelines-task-lib/task");
 const AnnotationUtility_1 = require("../common/RestUtilities/AnnotationUtility");
+const core = __importStar(require("@actions/core"));
 class WebAppDeploymentProvider {
     constructor(taskParams) {
         this.taskParams = taskParams;
@@ -34,12 +41,12 @@ class WebAppDeploymentProvider {
                 yield AnnotationUtility_1.addAnnotation(this.taskParams.endpoint, this.appService, isDeploymentSuccess);
                 if (updateStatus) {
                     this.activeDeploymentID = yield this.kuduServiceUtility.updateDeploymentStatus(isDeploymentSuccess, null, { 'type': 'Deployment', slotName: this.appService.getSlot() });
-                    tl.debug('Active DeploymentId :' + this.activeDeploymentID);
+                    core.debug('Active DeploymentId :' + this.activeDeploymentID);
                 }
             }
             let appServiceApplicationUrl = yield this.appServiceUtility.getApplicationURL();
-            console.log(tl.loc('AppServiceApplicationURL', appServiceApplicationUrl));
-            tl.setVariable('AppServiceApplicationUrl', appServiceApplicationUrl);
+            console.log('AppServiceApplicationURL' + appServiceApplicationUrl);
+            core.exportVariable('AppServiceApplicationUrl', appServiceApplicationUrl);
         });
     }
 }
