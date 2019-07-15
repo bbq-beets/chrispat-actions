9910a71224f59c86ae9c10d23f7e43d980ceecb9
Sumiran Aggarwal <suaggar@microsoft.com>
2019-07-15 14:29:10 +0530

    df

--- azure-login-docker/entrypoint.sh
+++ azure-login-docker/entrypoint.sh
@@ -3,5 +3,5 @@
 set -e
 
 args
-RESOURCE_GROUP_NAME=$(az resource list -n "suaggarTest1" --resource-type "Microsoft.Web/Sites" --query '[0].resourceGroup' | xargs)
-echo `${RESOURCE_GROUP_NAME}` >&2
+#RESOURCE_GROUP_NAME=$(az resource list -n "suaggarTest1" --resource-type "Microsoft.Web/Sites" --query '[0].resourceGroup' | xargs)
+#echo `${RESOURCE_GROUP_NAME}` >&2
